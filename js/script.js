4ae8a8ce069ea32d6d8cc4bc63b7dec4baf83efb
akhila-baiju <101245217+akhila-baiju@users.noreply.github.com>
2022-04-06 00:37:56 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function validate()
+{
+    var uname=document.getElementById("uname").value;
+    var pass=document.getElementById("pwd").value;
+    checkFunction(uname,pass,myCallback);
+}
+
+function checkFunction(user,pwd,myCallback)
+{
+    if(user == "admin" && pwd == "12345")
+    {
+        myCallback(true);
+    }
+    else{
+        myCallback(false);
+    }
+}
+
+function myCallback(result)
+{
+    if(result)
+    {
+        window.open("./welcome.html");
+        //window.location = "";
+    }else{
+        window.open("./index.html");
+    }
+}
